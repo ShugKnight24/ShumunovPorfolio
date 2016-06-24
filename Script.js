9c7ec469f6de571eba1718c8a178f9f51fb62559
ShugKnight24 <SShumunov@gmail.com>
2016-06-23 23:52:36 -0400

    Changed the text changer and footer

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
-function textChange(event) {
-	text
-	var name = document.getElementById("Name").value;
-	document.getElementById("text").innerHTML = 'Your name is: ' + name;
-}
+  function textChange() {
+  	var input = document.getElementById("changer");
+    var text = input.value;
+    
+    if (!text)  {
+    	alert("Hey smarty pants! You have to type something in first if you want to see the magic!");
+      return;
+    } 
+    input.value = text.toUpperCase();
+	}
